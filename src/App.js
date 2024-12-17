de3eb35bb9a308c92e47659cabcdd0920e2c5e73
Shekhar0050M <shekhar0050m@gmail.com>
2024-12-17 20:12:46 +0530

    12/17/2024 20:12

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-const App = () => {
-  const items = ['Hello', 'World'];
+import Home from './pages/Home';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
+const App = () => {
+  
   return (
-    <div>
-      {items.map(item => <p>{item}</p> )}
-    </div>
-  );
+    <>
+    <Navbar/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
+    <Footer/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,27 @@
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="collapse navbar-collapse">
+                <a href="/" className="navbar-brand">Decentralized Voting Platform</a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="login" className="nav-link">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="register" className="nav-link">Register</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="container-fluid row justify-content-end">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
